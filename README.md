f6a18c526489e70229919ec3f1f1a843979706d7
RobDWaller <rob@statuspeople.com>
2018-03-21 08:11:05 +0000

    Began strucuring README file and added more admin controller tests.

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
 # Mongolium Core
-[![Build Status](https://travis-ci.org/mongolium/mongolium.svg?branch=master)](https://travis-ci.org/mongolium/mongolium)
+[![Build Status](https://travis-ci.org/mongolium/mongolium.svg?branch=master)](https://travis-ci.org/mongolium/mongolium) [![codecov](https://codecov.io/gh/mongolium/mongolium/branch/master/graph/badge.svg)](https://codecov.io/gh/mongolium/mongolium)
+[![StyleCI](https://styleci.io/repos/123986051/shield?branch=master)](https://styleci.io/repos/123986051)
 
-A PHP and MongoDB based CMS that delivers data via APIs and services only.
+A PHP and MongoDB based CMS for developers that delivers data via APIs and services only.
+
+## Overview
+
+Mongolium is broken down into four basic layers.
+
+- **Controllers:** manages restful requests, interacts with the services layer and returns JSON for the API.
+- **Services:** core business logic, provides application interfaces for .
+- **Models:** strongly defined value objects / entities that provide structure to the Mongo objects and collections.
+- **ORM:** responsible for hydrating the model entities with data returned from Mongo before passing them back to the services layer.
+
+## Endpoints
+
+## Installation
+
+## Extension  
+
+## Author
--- tests/Core/Unit/Controllers/AdminTest.php
+++ tests/Core/Unit/Controllers/AdminTest.php
@@ -9,6 +9,7 @@ use Slim\Http\Response as SlimResponse;
 use Slim\Http\Request;
 use PHPUnit\Framework\TestCase;
 use Tests\Core\Helper\Admin as AdminHelper;
+use ReallySimple\Collection;
 use Mockery as m;
 
 class AdminTest extends TestCase
@@ -50,5 +51,132 @@ class AdminTest extends TestCase
         $this->assertRegExp('|"id":"' . $admin['id'] . '"|', $data);
         $this->assertRegExp('|"username":"' . $admin['username'] . '"|', $data);
         $this->assertRegExp('|"type":"' . $admin['type'] . '"|', $data);
+    }
+
+    public function testAdminUpdate()
+    {
+        $admin = AdminHelper::admin(true);
+
+        $adminService = m::mock(AdminService::class);
+        $adminService->shouldReceive('update')->once()->andReturn(
+            AdminModel::hydrate($admin)
+        );
+
+        $adminController = new Admin($adminService);
+
+        $request = m::mock(Request::class);
+        $request->shouldReceive('getParsedBody')->once()->andReturn([]);
+
+        $response = new SlimResponse(200);
+
+        $result = $adminController->update($request, $response);
+
+        $this->assertInstanceOf(SlimResponse::class, $result);
+
+        $this->assertEquals(200, $result->getStatusCode());
+        $this->assertStringStartsWith('application/json', $result->getHeaderLine('Content-type'));
+
+        $data = $result->__toString();
+
+        $this->assertRegExp('|"id":"' . $admin['id'] . '"|', $data);
+        $this->assertRegExp('|"username":"' . $admin['username'] . '"|', $data);
+        $this->assertRegExp('|"type":"' . $admin['type'] . '"|', $data);
+    }
+
+    public function testAdminRead()
+    {
+        $admins[] = AdminModel::hydrate(AdminHelper::admin(true));
+        $admins[] = AdminModel::hydrate(AdminHelper::admin(true));
+        $admins[] = AdminModel::hydrate(AdminHelper::admin(true));
+        $admins[] = AdminModel::hydrate(AdminHelper::admin(true));
+
+        $adminService = m::mock(AdminService::class);
+        $adminService->shouldReceive('all')->once()->andReturn(
+            new Collection($admins)
+        );
+
+        $adminController = new Admin($adminService);
+
+        $request = m::mock(Request::class);
+        $request->shouldReceive('getParsedBody')->once()->andReturn([]);
+
+        $response = new SlimResponse(200);
+
+        $result = $adminController->read($request, $response);
+
+        $this->assertInstanceOf(SlimResponse::class, $result);
+
+        $this->assertEquals(200, $result->getStatusCode());
+        $this->assertStringStartsWith('application/json', $result->getHeaderLine('Content-type'));
+
+        $data = $result->__toString();
+
+        $this->assertRegExp('|"id":"|', $data);
+        $this->assertRegExp('|"type":"admin"|', $data);
+        $this->assertRegExp('|"data":\[|', $data);
+        $this->assertRegExp('|"links":{|', $data);
+    }
+
+    public function testAdminReadOne()
+    {
+        $admin = AdminHelper::admin(true);
+
+        $adminService = m::mock(AdminService::class);
+        $adminService->shouldReceive('getAdmin')->once()->andReturn(
+            AdminModel::hydrate($admin)
+        );
+
+        $adminController = new Admin($adminService);
+
+        $request = m::mock(Request::class);
+
+        $response = new SlimResponse(200);
+
+        $args = ['id' => 1];
+
+        $result = $adminController->readOne($request, $response, $args);
+
+        $this->assertInstanceOf(SlimResponse::class, $result);
+
+        $this->assertEquals(200, $result->getStatusCode());
+        $this->assertStringStartsWith('application/json', $result->getHeaderLine('Content-type'));
+
+        $data = $result->__toString();
+
+        $this->assertRegExp('|"id":"' . $admin['id'] . '"|', $data);
+        $this->assertRegExp('|"username":"' . $admin['username'] . '"|', $data);
+        $this->assertRegExp('|"type":"' . $admin['type'] . '"|', $data);
+        $this->assertRegExp('|"data":{"|', $data);
+    }
+
+    public function testAdminDelete()
+    {
+        $admin = AdminHelper::admin(true);
+
+        $adminService = m::mock(AdminService::class);
+        $adminService->shouldReceive('delete')->once()->andReturn(
+            true
+        );
+
+        $adminController = new Admin($adminService);
+
+        $request = m::mock(Request::class);
+
+        $response = new SlimResponse(200);
+
+        $args = ['id' => 1];
+
+        $result = $adminController->delete($request, $response, $args);
+
+        $this->assertInstanceOf(SlimResponse::class, $result);
+
+        $this->assertEquals(200, $result->getStatusCode());
+        $this->assertStringStartsWith('application/json', $result->getHeaderLine('Content-type'));
+
+        $data = $result->__toString();
+
+        $this->assertRegExp('|"id":"|', $data);
+        $this->assertRegExp('|"message":"Admin deleted\."|', $data);
+        $this->assertRegExp('|"data":{"|', $data);
     }
 }
